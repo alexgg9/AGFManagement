dee5ebef9d65c704260d5324679a30f505d2c05c
alexgg9 <alexgalvezgarciaqt@gmail.com>
2023-05-04 11:23:10 +0200

    DAOS

--- src/main/java/model/DAO/EventoDAO.java
+++ src/main/java/model/DAO/EventoDAO.java
@@ -1,5 +1,38 @@
 package model.DAO;
 
-public class EventoDAO {
+import java.sql.SQLException;
+import java.util.List;
+
+public class EventoDAO implements DAO{
+
+	@Override
+	public void close() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List findAll() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object findByDni(String id) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object save(Object entity) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Object entity) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- src/main/java/model/DAO/MatchmakerDAO.java
+++ src/main/java/model/DAO/MatchmakerDAO.java
@@ -1,5 +1,38 @@
 package model.DAO;
 
-public class MatchmakerDAO {
+import java.sql.SQLException;
+import java.util.List;
+
+public class MatchmakerDAO implements DAO{
+
+	@Override
+	public void close() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List findAll() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object findByDni(String id) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object save(Object entity) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Object entity) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
 
 }

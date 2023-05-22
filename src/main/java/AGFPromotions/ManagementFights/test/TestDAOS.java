a41d1e94dde5fb872ab59efe1dfc77fe81661ddc
alexgg9 <alexgalvezgarciaqt@gmail.com>
2023-05-22 21:59:28 +0200

    cambioTest

--- src/main/java/AGFPromotions/ManagementFights/test/TestDAOS.java
+++ src/main/java/AGFPromotions/ManagementFights/test/TestDAOS.java
@@ -14,14 +14,13 @@ import java.sql.Date;
 public class TestDAOS {
 	public static void main(String[] args) throws ParseException {
 		
+		
 		Peleador p = new Peleador("1212","Ilia","Topuria",26,Genero.MALE,66, 170,"13-0-0","Georgia",Background.GRAPPLING);
 		Peleador p1 = new Peleador("1313","Brian","Ortega",38,Genero.MALE,66, 172,"15-4-0","EEUU",Background.GRAPPLING);
 		Peleador p2 = new Peleador("1414","Yair","Rodriguez",30,Genero.MALE,66, 185,"14-3-0","Mexico",Background.STRIKING);
 		Matchmaker m = new Matchmaker("12345678C","Cesar","Alonso","WOW","CesarAlonso10","pass1");
 		
 		Date d = null;
-		
-		//Evento ev = new Evento(2,"Fight Night","Apex","Las Vegas","EEUU",Modalidad.MMA,d,p,p1,m);
 		
 		PeleadorDAO pdao = new PeleadorDAO();
 		MatchmakerDAO mdao = new MatchmakerDAO();
